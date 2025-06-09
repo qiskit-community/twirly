ec4daa09c022d0773f29c5279d375af34ad225ea
SamFerracin <sam.ferracin@ibm.com>
2025-06-09 12:14:34 +0200

    dependencies

--- twirly/_version.py
+++ twirly/_version.py
@@ -16,5 +16,5 @@ __version__: str
 __version_tuple__: VERSION_TUPLE
 version_tuple: VERSION_TUPLE
 
-__version__ = version = "0.1.dev19+g4817cd9.d20250609"
-__version_tuple__ = version_tuple = (0, 1, "dev19", "g4817cd9.d20250609")
+__version__ = version = "0.1.dev20+g3273997.d20250609"
+__version_tuple__ = version_tuple = (0, 1, "dev20", "g3273997.d20250609")
